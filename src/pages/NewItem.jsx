f617035d8e88769211a72904c80053a627a17e67
yoonezi <jeji3391@naver.com>
2023-03-09 14:42:49 +0900

    Feat:NewItem Filter Category

--- src/pages/NewItem.jsx
+++ src/pages/NewItem.jsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Layouts from "../Common/Layout";
 import { productlist } from "../ProductDummy";
 import NewItemList from "../components/NewItemList";
+import ViewContent from "./Product";
 
 export default function NewItem() {
   return (
@@ -20,7 +21,27 @@ export default function NewItem() {
         </NewItemBanner>
         <NewItemTitle>신상품</NewItemTitle>
         <NewItemBottom>
-          <NewItemFilter>필터</NewItemFilter>
+          <NewItemFilter>
+            <FilterTitle>
+              <span>필터</span>
+            </FilterTitle>
+            <div>
+              <FilterCategory>
+                <button>
+                  <h3>카테고리</h3>
+                </button>
+                <FilterClick>
+                  <ViewContent />
+                  {/* <li>
+                    <a href="/">
+                      <span>국·반찬·메인요리</span>
+                      <h3>32</h3>
+                    </a>
+                  </li> */}
+                </FilterClick>
+              </FilterCategory>
+            </div>
+          </NewItemFilter>
           <NewItemMain>
             <NewItemMainTop>
               <MainTopTotal>총 235건</MainTopTotal>
@@ -85,7 +106,7 @@ export const NewItemTitle = styled.h3`
   margin-top: 50px;
   font-weight: 500;
   font-size: 28px;
-  color: rgb(51, 51, 51);
+  color: #333333;
   line-height: 35px;
   letter-spacing: -1px;
   text-align: center;
@@ -104,18 +125,6 @@ export const NewItemBottom = styled.div`
   width: 1050px;
   margin-top: 50px;
   margin-bottom: 75px;
-`;
-
-export const NewItemFilter = styled.div`
-  position: sticky;
-  width: 220px;
-  flex-shrink: 0;
-  height: 100%;
-  max-height: calc(100vh - 120px);
-  top: 80px;
-  margin-right: 47px;
-  border-bottom: 1px solid rgb(238, 238, 238);
-  overflow: hidden scroll;
 `;
 
 export const NewItemMain = styled.div`
@@ -167,4 +176,107 @@ export const HeadeVertical = styled.div`
   height: 13px;
   margin: 0px 8px;
   background-color: rgb(217, 217, 217);
+`;
+
+export const NewItemFilter = styled.div`
+  position: sticky;
+  width: 220px;
+  flex-shrink: 0;
+  height: 100%;
+  max-height: calc(100vh - 120px);
+  top: 80px;
+  margin-right: 47px;
+
+  /* overflow: hidden scroll; */
+`;
+
+export const FilterTitle = styled.div`
+  position: sticky;
+  top: 0px;
+  display: flex;
+  z-index: 20;
+  -webkit-box-pack: justify;
+  /* justify-content: space-between; */
+  -webkit-box-align: center;
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom: 1px solid rgb(238, 238, 238);
+  line-height: 20px;
+
+  & span {
+    font-weight: 500;
+    font-size: 15px;
+    color: rgb(51, 51, 51);
+  }
+`;
+
+export const FilterClick = styled.ul`
+  max-height: 100vh;
+  opacity: 1;
+  list-style-type: none;
+  display: block;
+  list-style-type: disc;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 40px;
+  font-size: 14px;
+  color: #333;
+
+  & li {
+    margin-top: 9px;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    margin-bottom: 18px;
+    & a {
+      background-color: transparent;
+      text-decoration: none;
+      color: inherit;
+      display: flex;
+      & span {
+        margin-right: 4px;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 17px;
+        color: rgb(51, 51, 51);
+        display: -webkit-box;
+        overflow: hidden;
+        word-break: break-all;
+        white-space: normal;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+      & h3 {
+        flex-shrink: 0;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 16px;
+        color: rgb(204, 204, 204);
+      }
+    }
+  }
+`;
+
+export const FilterCategory = styled.div`
+  border-bottom: 1px solid rgb(238, 238, 238);
+  & button {
+    display: flex;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    -webkit-box-align: center;
+    align-items: center;
+    width: 100%;
+    height: 52px;
+    overflow: hidden;
+    background-color: transparent;
+    border: none;
+    padding: 0;
+    font-weight: 500;
+    font-size: 15px;
+    color: rgb(51, 51, 51);
+  }
+  & ul {
+  }
 `;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Product() {
+const ViewContent = () => {
+  const [hanpy, setHanpy] = useState(false);
+  const handleView = () => {
+    setHanpy(!hanpy);
+  };
   return (
-    <div className="page-container" style={{ fontSize: "32px" }}>
-      Product
-    </div>
+    <>
+      <p>ViewContent 파일</p>
+      <button onClick={handleView}>ViewControl</button>
+      {hanpy ? (
+        <ul>
+          <li>
+            <a href="/">
+              <span>국·반찬·메인요리</span>
+              <h3>32</h3>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <span>국·반찬·메인요리</span>
+              <h3>32</h3>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <span>국·반찬·메인요리</span>
+              <h3>32</h3>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <span>국·반찬·메인요리</span>
+              <h3>32</h3>
+            </a>
+          </li>
+        </ul>
+      ) : (
+        <></>
+      )}
+    </>
   );
-}
+};
+
+export default ViewContent;
